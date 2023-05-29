aa42b8050946a6fb4df98f502b2a9b8a28254d58
strix_uralensis <dhurarara1229@outlook.jp>
2023-05-30 02:02:54 +0900

    add fotune bot

--- bolt.js
+++ bolt.js
@@ -1,17 +1,24 @@
 'use strict';
 const bolt = require('@slack/bolt');
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const app = new bolt.App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-  logLevel: 'debug'
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+    logLevel: 'debug',
 });
 
 app.message(/hello/i, ({message, say}) => {
-  say('こんにちは！');
+    say(`こんにちは！ <@${message.user}>さん`);
+});
+
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+
+app.message(/おみくじ/i, ({message, say}) => {
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    say(`${lot}, <@${message.user}>`);
 });
 
 app.start();
